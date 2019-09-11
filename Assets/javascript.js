3485fe41c3a8e2db1c5372b672be54d34c629031
Tom Cox <thomasxcox731@gmail.com>
2019-09-11 12:41:34 -0500

    Initial javascript pseudocoding started.

---
+++ Assets/javascript.js
@@ -0,0 +1,6 @@
+//Variables: total score = "", ...//
+var totalScore = "";
+
+function restartGame(){
+    //new random number generated, new random crystal numbers generated, "total score" and the counter reset to 0.//
+}
