382b4ecac29a68d856e08a92ea296ac2f6ada9b6
Tom Cox <thomasxcox731@gmail.com>
2019-09-12 09:51:24 -0500

    I reorganized some elements in the js so that the crystals have a new random number whenever the site refreshes, so they're not always random like they were previously.

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,22 +1,33 @@
 //Variables
-// var crystalChoices = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
-// var crystalNumber = crystalChoices[Math.floor(Math.random() * crystalChoices.length)];
-//create random compNumber
-var compNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-console.log(compNumber);
-$("#compNumber").text("Computer Generated Number: " + compNumber);
 
 var totalScore = 0;
 var wins = 0;
 var losses = 0;
 
+//create random crystal numbers
+var crystalNumber1 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+$("#image1").attr("data-value", crystalNumber1);
+var crystalNumber2 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+$("#image2").attr("data-value", crystalNumber2);
+var crystalNumber3 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+$("#image3").attr("data-value", crystalNumber3);
+var crystalNumber4 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+$("#image4").attr("data-value", crystalNumber4);
+
+//create random compNumber
+var compNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+console.log(compNumber);
+$("#compNumber").text("Computer Generated Number: " + compNumber);
+
 //create randomizer for crystalNumber
 //function - if a button is clicked, add its number to "total score"
 $(".button").on("click", function () {
-    var crystalNumber = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-    console.log("crystal number: " + crystalNumber);
+    // var crystalNumber = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    // console.log("crystal number: " + crystalNumber);
+    var num = +$(this).attr("data-value");
+    console.log(num);
     //see image's number value to totalScore 
-    totalScore = crystalNumber + totalScore;
+    totalScore = num + totalScore;
     console.log("total score: " + totalScore);
     $("#totalScore2").text(totalScore);
     //if total score = compNumber, win++, call restart game()
@@ -32,17 +43,19 @@ $(".button").on("click", function () {
         restartGame();
     }
 })
-function restartGame() { 
-        //new random number generated, new random crystal numbers generated, "total score" and the counter reset to 0.//
-        var compNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-        console.log(compNumber);
-        $("#compNumber").text("Computer Generated Number: " + compNumber);
-        totalScore = 0;
+function restartGame() {
+    //new random number generated, new random crystal numbers generated, "total score" and the counter reset to 0.//
+    var compNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+    console.log(compNumber);
+    $("#compNumber").text("Computer Generated Number: " + compNumber);
+    var crystalNumber1 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    $("#image1").attr("data-value", crystalNumber1);
+    var crystalNumber2 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    $("#image2").attr("data-value", crystalNumber2);
+    var crystalNumber3 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    $("#image3").attr("data-value", crystalNumber3);
+    var crystalNumber4 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    $("#image4").attr("data-value", crystalNumber4);
+    totalScore = 0;
 
-    }
-//set text of scoreboard 
-// var winsText = $("#wins").text("Wins: " + wins);
-// var lossesText = $("#losses").text("Losses: " + losses);
-// var scoreText = $("totalScore2").text(totalScore);
-// var crystalChoices = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
-// var crystalNumber = crystalChoices[Math.floor(Math.random() * crystalChoices.length)];
+}
