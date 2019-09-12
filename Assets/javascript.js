44f9b062f65fc6109aaa3268e939bf15aa89f016
Tom Cox <thomasxcox731@gmail.com>
2019-09-11 19:28:21 -0500

    More functions added to the javascript. I still need to figure out how to generate the random numbers. I have taken care of what happens once they're clicked

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,18 +1,34 @@
 //Variables
-var compNumber = Math.floor(Math.random() * 120);
 var crystalNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
 var totalScore = "";
 var wins = 0;
 var losses = 0;
 //create random compNumber
-function setTimout () {
-    compNumber = Math.floor(Math.random() * 19);
-    $("#compNumber").text(compNumber);
+function randomComp() {
+    var compNumber = Math.floor(Math.random() * 120);
+    $("#compNumber").text("Computer Generated Number: " + compNumber);
 }
 //create randomizer for crystalNumber
+
 //function - if a button is clicked, add its number to "total score"
-//function - if total score = compNumber, win++, call restart game()
-//function - if total score > compNumber, losses++, call restart game()
-function restartGame(){
-    //new random number generated, new random crystal numbers generated, "total score" and the counter reset to 0.//
+$("#button").on("click", function () {
+    //send image's number value to totalScore 
+    totalScore = crystalNumber + totalScore;
+    //if total score = compNumber, win++, call restart game()
+    if (totalScore === compNumber) {
+        wins++;
+        restartGame();
+    }
+    //function - if total score > compNumber, losses++, call restart game()
+    if (totalScore > compNumber) {
+        losses++;
+        restartGame();
+    }
 }
+function restartGame() {
+        //new random number generated, new random crystal numbers generated, "total score" and the counter reset to 0.//
+    }
+    
+//set text of scoreboard 
+var winsText = $("#wins").text("Wins: " + wins);
+var lossesText = $("#losses").text("Losses" + losses);
