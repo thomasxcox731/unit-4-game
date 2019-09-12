1a0296c4794e329860e82aa1242484044ed81478
Tom Cox <thomasxcox731@gmail.com>
2019-09-11 19:59:17 -0500

    The crystal image number randomizer works now! It's showing up in the console but not on the page.

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -12,20 +12,23 @@ var losses = 0;
 
 //create randomizer for crystalNumber
 //function - if a button is clicked, add its number to "total score"
-$("#button").on("click", function () {
+$(".button").on("click", function () {
     var crystalNumber = Math.floor(Math.random() * (12 - 1 + 1) + 1);
     console.log(crystalNumber);
-    //send image's number value to totalScore 
+    //see image's number value to totalScore 
     totalScore = crystalNumber + totalScore;
     console.log(totalScore);
+    $("totalScore2").text(totalScore);
     //if total score = compNumber, win++, call restart game()
     if (totalScore === compNumber) {
         wins++;
+        $("#wins").text("Wins: " + wins);
         restartGame();
     }
     //function - if total score > compNumber, losses++, call restart game()
     if (totalScore > compNumber) {
         losses++;
+        $("#losses").text("Losses: " + losses);
         restartGame();
     }
 })
@@ -38,8 +41,8 @@ function restartGame() {
 
     }
 //set text of scoreboard 
-var winsText = $("#wins").text("Wins: " + wins);
-var lossesText = $("#losses").text("Losses" + losses);
-var scoreText = $("totalScore2").text(totalScore);
+// var winsText = $("#wins").text("Wins: " + wins);
+// var lossesText = $("#losses").text("Losses: " + losses);
+// var scoreText = $("totalScore2").text(totalScore);
 // var crystalChoices = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
 // var crystalNumber = crystalChoices[Math.floor(Math.random() * crystalChoices.length)];
